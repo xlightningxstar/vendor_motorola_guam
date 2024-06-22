44f5ae4894daeb2763c8bb398fd247268d0b7636
josexda <josegustavo6@protonmail.com>
2024-06-21 21:23:06 -0300

    guam: remove firmware images
    Change-Id: I650cc69ffe5bc201029851a0fa25246154768802

--- Android.mk
+++ Android.mk
@@ -6,22 +6,4 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),guam)
 
-$(call add-radio-file-sha1-checked,radio/bluetooth.img,0610a122e98e9bf9415551d445847a0c093bee27)
-$(call add-radio-file-sha1-checked,radio/modem.img,55f2befc39a90774ec337660046890f35ae0b50c)
-$(call add-radio-file-sha1-checked,radio/abl.img,1d3f6b2e6764b5ad6ae19e3cc737a66779fd266d)
-$(call add-radio-file-sha1-checked,radio/devcfg.img,6a338d179fa32261ba1b3d69d0a5f8bd7bc1c08e)
-$(call add-radio-file-sha1-checked,radio/dsp.img,59957f1abe929de1e6bb676a0cebad4907f0f2d0)
-$(call add-radio-file-sha1-checked,radio/fsg.img,fe36fec129ae428bd9608416b4dc735b66289314)
-$(call add-radio-file-sha1-checked,radio/hyp.img,16d050bd18e30b0f7911239c9c0bfb4eac815197)
-$(call add-radio-file-sha1-checked,radio/keymaster.img,330d3c9f16eaa7d6120940c985f4a694fcbe8cd0)
-$(call add-radio-file-sha1-checked,radio/logo.img,89f66f3d74501ab6fbfbafbac44fd4ef7d4f32ff)
-$(call add-radio-file-sha1-checked,radio/prov.img,d42c095199b29d49b9ebc8198a4d54002506edab)
-$(call add-radio-file-sha1-checked,radio/qupfw.img,e9527d957d6a371bd3a598b6e9c125579f565a05)
-$(call add-radio-file-sha1-checked,radio/rpm.img,9cd9e0c1237821b5846da2142ded9b6c436d3d67)
-$(call add-radio-file-sha1-checked,radio/storsec.img,fbb705c8ab71f72a7984a643207088d8d9dc5305)
-$(call add-radio-file-sha1-checked,radio/tz.img,349e25a4e050bec4ee2f578fd4a80bab7fd8ca98)
-$(call add-radio-file-sha1-checked,radio/uefisecapp.img,a46be930361141ce80d7e1c88b2e886e02f49fde)
-$(call add-radio-file-sha1-checked,radio/xbl.img,f85ee3c8289177e225104bfcb53790982c451cbf)
-$(call add-radio-file-sha1-checked,radio/xbl_config.img,4b35beb54e1bd4ab2872e61c969a92bf46b40d80)
-
 endif
